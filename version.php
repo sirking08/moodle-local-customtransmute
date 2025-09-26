c2ef159ab1e72bfe579f805a9ac5b5708992ce82
Ezekiel Lozano <sirking08@gmail.com>
2025-09-26 22:07:05 +0800

    Fixed Version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_customtransmute';
-$plugin->version = 2025092400;
+$plugin->version = 2025092401;
 $plugin->requires = 2022112800; // Requires Moodle 4.1+
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0.0';
+$plugin->release = '1.0.1';
